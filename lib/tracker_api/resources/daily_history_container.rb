e6f9ec0a3531f1d56c2db39dced5d970fc37c0fe
taykangsheng <taykangsheng@spgroup.com.sg>
2017-05-09 17:14:48 +0800

    add Resource DailyHistoryContainer

--- lib/tracker_api/resources/daily_history_container.rb
+++ lib/tracker_api/resources/daily_history_container.rb
@@ -5,16 +5,8 @@ module TrackerApi
 
       attribute :project_id, Integer
       attribute :iteration_number, Integer
-      attribute :total_cycle_time, Integer
-      attribute :started_time, Integer
-      attribute :started_count, Integer
-      attribute :finished_time, Integer
-      attribute :finished_count, Integer
-      attribute :delivered_time, Integer
-      attribute :delivered_count, Integer
-      attribute :rejected_time, Integer
-      attribute :rejected_count, Integer
-      attribute :story_id, Integer
+      attribute :header, [String]
+      attribute :data, [Enumerable]
       attribute :kind, String
 
     end
